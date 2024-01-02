1a2b22aa3b90afac555ad4772b95195d4ce5f2f1
Aram Zucker-Scharff <Aram.Zucker-Scharff@washpost.com>
2024-01-02 11:47:57 -0500

    Fix up the links with blank and different colors

--- functions.php
+++ functions.php
@@ -198,26 +198,19 @@ function ilovewp_customizer_define_general_sections_child( $sections ) {
     return array_merge( $sections, $general_sections );
 }
 
+function alter_link_to_htmx($toReplace, $content){
+	$baseUrl = parse_url(home_url(), PHP_URL_HOST);
+	//$urlParts = explode('.', $baseUrl);
+
+	if (preg_match('/'.preg_quote($baseUrl).'/',$toReplace)){
+		$replacement = preg_replace('/(\<a.*?)(\>)(.*?\/a\>.*?)/','$1 rel="internal"$2$3',$toReplace);
+		// replaces the current link with the $replacement string
+		$content = str_ireplace($toReplace,$replacement,$content);
+	}
+	return $content;
+}
+
 add_filter( 'ilovewp_customizer_sections', 'ilovewp_customizer_define_general_sections_child', 11 );
 
-add_filter( 'the_content', 'ex1_the_content_filter' );
-function ex1_the_content_filter($content) {
-    // finds all links in your content
-    preg_match_all('/(\<a href=.*?a>)/',$content,$matches, PREG_SET_ORDER);
-
-    // loop through all matches
-    foreach($matches as $m){
-        // potential link to be replaced...
-        $toReplace = $m[0];
-
-        // if current link does not already have target="{whatever}"
-        // You can add whatever additional "IF" you require to this one
-        if (!preg_match('/target\s*\=/',$toReplace)){
-            // adds target="_blank" to the current link
-            $replacement = preg_replace('/(\<a.*?)(\>)(.*?\/a\>.*?)/','$1 target="_blank"$2$3',$toReplace);
-            // replaces the current link with the $replacement string
-            $content = str_ireplace($toReplace,$replacement,$content);
-        }
-    }
-      return $content;
-}
+require_once( get_theme_file_path('/supporting-functions/post-treatments.php') );
+require_once( get_theme_file_path('/supporting-functions/link-treatments.php') );
